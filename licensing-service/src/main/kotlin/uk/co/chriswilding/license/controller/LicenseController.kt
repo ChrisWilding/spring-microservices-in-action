4e33b01c66eadcae859d52c4709daaba12b53bfe
Chris Wilding <hello@chriswilding.co.uk>
2021-11-06 15:52:52 +0000

    Add HATEOAS

--- licensing-service/src/main/kotlin/uk/co/chriswilding/license/controller/LicenseController.kt
+++ licensing-service/src/main/kotlin/uk/co/chriswilding/license/controller/LicenseController.kt
@@ -1,7 +1,15 @@
 package uk.co.chriswilding.license.controller
 
+import org.springframework.hateoas.server.mvc.linkTo
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import uk.co.chriswilding.license.model.License
 import uk.co.chriswilding.license.service.LicenseService
 
@@ -16,7 +24,7 @@ class LicenseController(private val licenseService: LicenseService) {
         return ResponseEntity.ok(licenseService.createLicense(organisationId, license))
     }
 
-    @DeleteMapping( "/{licenseId}")
+    @DeleteMapping("/{licenseId}")
     fun deleteLicense(
         @PathVariable("organisationId") organisationId: String,
         @PathVariable("licenseId") licenseId: String,
@@ -24,12 +32,28 @@ class LicenseController(private val licenseService: LicenseService) {
         return ResponseEntity.ok(licenseService.deleteLicense(organisationId, licenseId));
     }
 
-    @GetMapping( "/{licenseId}")
+    @GetMapping("/{licenseId}")
     fun getLicense(
         @PathVariable("organisationId") organisationId: String,
         @PathVariable("licenseId") licenseId: String,
     ): ResponseEntity<License> {
         val license = licenseService.getLicense(organisationId, licenseId)
+
+        license.add(
+            linkTo<LicenseController> {
+                this.getLicense(organisationId, license.licenseId)
+            }.withSelfRel(),
+            linkTo<LicenseController> {
+                this.createLicense(organisationId, license)
+            }.withRel("createLicense"),
+            linkTo<LicenseController> {
+                this.updateLicense(organisationId, license)
+            }.withRel("updateLicense"),
+            linkTo<LicenseController> {
+                this.deleteLicense(organisationId, license.licenseId)
+            }.withRel("deleteLicense")
+        )
+
         return ResponseEntity.ok(license)
     }
 
--- licensing-service/src/main/kotlin/uk/co/chriswilding/license/model/License.kt
+++ licensing-service/src/main/kotlin/uk/co/chriswilding/license/model/License.kt
@@ -1,5 +1,7 @@
 package uk.co.chriswilding.license.model
 
+import org.springframework.hateoas.RepresentationModel;
+
 data class License(
     val id: Int,
     val licenseId: String,
@@ -7,4 +9,4 @@ data class License(
     val organisationId: String,
     val productName: String,
     val licenseType: String,
-)
+): RepresentationModel<License>()
